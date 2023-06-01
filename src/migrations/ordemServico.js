be9e17038836d34ae766a1a88dab7a1620de7764
evertonhazevedo <hinckley.everton@gmail.com>
2023-06-01 18:45:13 -0300

    Ajuste estrutura banco de dados

--- src/migrations/ordemServico.js
+++ src/migrations/ordemServico.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../models/db');
-const tabelaServico = require('./servico');
 const tabelaCliente = require('./cliente');
 const tabelaVeiculo = require('./veiculos');
-const sequelize = require('../models/db');
 
 //criando tabela Ordem de Servicos
 
@@ -18,20 +16,20 @@ const tabelaOrdemServico = db.define('ordemServico', {
 
 }, { freezeTableName: true });
 
-// Relacionamento 1-1 'cd_servico':
+// // Relacionamento 1-1 'cd_servico':
 
-tabelaOrdemServico.belongsTo(tabelaServico, {
-  constraint: true,
-  foreignKey: 'cd_servico',
-  allowNull: false
+// tabelaOrdemServico.belongsTo(tabelaServico, {
+//   constraint: true,
+//   foreignKey: 'cd_servico',
+//   allowNull: false
 
-});
+// });
 
-// relacionamento 1 - vários 'cd_servico':
+// // relacionamento 1 - vários 'cd_servico':
 
-tabelaServico.hasMany(tabelaOrdemServico, {
-  foreignKey: 'cd_servico'
-});
+// tabelaServico.hasMany(tabelaOrdemServico, {
+//   foreignKey: 'cd_servico'
+// });
 
 // Relacionamento 1-1 'cd_cliente':
 
@@ -64,4 +62,5 @@ tabelaVeiculo.hasMany(tabelaOrdemServico, {
 // Método para verificar se tabela já existe. Caso nao, irá criar tabela.
 
 tabelaOrdemServico.sync();
+
 module.exports = tabelaOrdemServico;
