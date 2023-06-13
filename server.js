fc4733ce8681bcc92f88f9960db2ea83fb6c30a4
evertonhazevedo <hinckley.everton@gmail.com>
2023-06-13 18:05:07 -0300

    rehorku 1

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use(express.static(path.join(__dirname, "/")));
 
 router.get('/', function (req, res) {
 
+  res.setHeader('Access-Control-Allow-Origin', baseUrl + '/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
